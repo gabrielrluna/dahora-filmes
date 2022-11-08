c6a79b15477629a04d9274ac8fc2e10e85484e87
Gabriel Luna <lunagabri@gmail.com>
2022-11-08 09:45:18 -0300

    Início da aplicação dos componentes

--- App.js
+++ App.js
@@ -1,118 +1,19 @@
-import { useFonts } from "expo-font";
-import {
-  Image,
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
 
-import { AntDesign } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
-import { Entypo } from "@expo/vector-icons";
-import logo from "./assets/images/logo.png";
-
-const corPrimaria = "#5451a6";
+import Home from "./src/screens/Home.js";
+import FormBusca from "./src/screens/FormBusca.js";
+import Favoritos from "./src/screens/Favoritos.js";
+import Sobre from "./src/screens/Sobre.js";
 
 const App = () => {
-  const [fonteCarregada] = useFonts({
-    monoton: require("./assets/fonts/Monoton-Regular.ttf"),
-  });
-
-  if (!fonteCarregada) return <Text>Fonte sendo fonteCarregada...</Text>;
-
   return (
-    <SafeAreaView style={estilos.container}>
-      <View style={estilos.viewLogo}>
-        <Image style={estilos.logo} source={logo} />
-        <Text style={estilos.tituloApp}>Dahora Filmes</Text>
-      </View>
-
-      <View style={estilos.viewBotoes}>
-        <Pressable style={estilos.botaoInicial}>
-          <Text style={estilos.textoBotao}>
-            <Ionicons name="search" size={16} color="white" />
-            Buscar Filmes
-          </Text>
-        </Pressable>
-        <Pressable style={estilos.botaoInicial}>
-          <Text style={estilos.textoBotao}>
-            <AntDesign name="star" size={16} color="gold" />
-            Favoritos
-          </Text>
-        </Pressable>
-      </View>
-
-      <View style={estilos.viewRodape}>
-        <Pressable style={estilos.botaoRodape}>
-          <Text style={estilos.textoBotao}>
-            <Entypo name="lock" size={16} color="white" />
-            Privacidade
-          </Text>
-        </Pressable>
-        <Pressable style={estilos.botaoRodape}>
-          <Text style={estilos.textoBotao}>
-            <AntDesign name="team" size={16} color="white" />
-            Sobre
-          </Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="default" />
+      <Sobre />
+    </>
   );
 };
 
 export default App;
 
-const estilos = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  viewLogo: {
-    flex: 3,
-    // width: "80%",
-    textAlign: "center",
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  tituloApp: {
-    fontSize: 32,
-    fontFamily: "monoton",
-    color: "#5451a6",
-  },
-  logo: {
-    width: 300,
-    height: 300,
-  },
-  viewBotoes: {
-    flex: 2,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "flex-start",
-    width: "80%",
-  },
-  botaoInicial: {
-    borderStyle: "solid",
-    borderWidth: 2,
-    padding: 16,
-    backgroundColor: corPrimaria,
-  },
-  botaoRodape: {
-    padding: 16,
-    backgroundColor: corPrimaria,
-  },
-  textoBotao: {
-    color: "white",
-  },
-  viewRodape: {
-    flex: 0.5,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    backgroundColor: corPrimaria,
-  },
-});
+const estilos = StyleSheet.create({});
---
+++ src/screens/Sobre.js
@@ -0,0 +1,45 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+const Sobre = () => {
+  return (
+    <SafeAreaView>
+      <Text style={estilos.titulo}>
+        Sobre o app <Text style={estilos.nomeApp}>DAHORA FILMES</Text>
+      </Text>
+      <Text style={estilos.texto}>
+        O <Text style={estilos.nomeApp}>DAHORA FILMES</Text> é um aplicativo com
+        a finalidade de permitir a busca por informações sobre filmes existentes
+        na base de dados pública disponibilizada pelo site The Movie Database
+        (TMDb).
+      </Text>
+      <Text style={estilos.texto}>
+        Ao localizar um filme, o usuário pode visualizar informações como
+        título, data de lançamento, nota média de avaliação e uma breve
+        descrição sobre o filme e, caso queira, salvar estas informações em uma
+        lista no próprio aplicativo para visualização posterior.
+      </Text>
+      <Text style={estilos.texto}>
+        O aplicativo poderá receber novos recursos conforme o feedback e demanda
+        dos usuários.
+      </Text>
+      <Text style={estilos.texto}>
+        <Text style={estilos.nomeApp}>DAHORA FILMES</Text> &copy; 2022
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default Sobre;
+
+const estilos = StyleSheet.create({
+  titulo: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginVertical: 8,
+  },
+
+  texto: {
+    marginVertical: 8,
+  },
+  nomeApp: { color: "#5461a6", fontWeight: "bold" },
+});
