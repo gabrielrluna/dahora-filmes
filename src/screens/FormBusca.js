78d6f4ded810fa38d84e631ad9ea39423052e9a4
Gabriel Luna <lunagabri@gmail.com>
2022-11-09 11:22:42 -0300

    Update FormBusca.js

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,20 +1,40 @@
-import { SafeAreaView, StyleSheet, Text, Pressable, View } from "react-native";
+import { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Button,
+  TextInput,
+  View,
+} from "react-native";
+
 import { Ionicons } from "@expo/vector-icons";
 
 const FormBusca = () => {
+  const [filme, setFilme] = useState("");
+  const textoDigitado = (valor) => {
+    setFilme(valor);
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Star Trek? O Poderoso Chefão? A Lagoa Azul?</Text>
-      <Text>Localize um filme que você viu ou gostaria de ver</Text>
-      <Pressable
-        style={estilos.botao}
-        // onPress={() => navigation.navigate("Home")}
-      >
-        <Text style={estilos.textoBotao}>
-          <Ionicons name="search" size={16} color="white" />
-          Procurar
-        </Text>
-      </Pressable>
+    <SafeAreaView style={estilos.container}>
+      <Text style={estilos.texto}>
+        Star Trek? O Poderoso Chefão? A Lagoa Azul?
+      </Text>
+      <Text style={estilos.texto}>
+        Localize um filme que você viu ou gostaria de ver
+      </Text>
+      <View style={estilos.pesquisa}>
+        <Ionicons name="film" size={60} color="black" />
+        <TextInput
+          style={estilos.input}
+          onChangeText={textoDigitado}
+          placeholder="Digite o nome do filme"
+          // value={text}
+        />
+      </View>
+
+      <Button color="#5451a6" title="Procurar" />
     </SafeAreaView>
   );
 };
@@ -22,12 +42,30 @@ const FormBusca = () => {
 export default FormBusca;
 
 const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
   botao: {
     backgroundColor: "#5451a6",
     textAlign: "center",
+    color: "black",
+    width: "95%",
+    alignContent: "center",
   },
-  textoBotao: {
-    textAlign: "center",
-    color: "white",
+  texto: {
+    fontSize: 16,
+    marginVertical: 8,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    width: "80%",
+    borderWidth: 1,
+    padding: 10,
+  },
+  pesquisa: {
+    flexDirection: "row",
+    alignContent: "space-between",
   },
 });
