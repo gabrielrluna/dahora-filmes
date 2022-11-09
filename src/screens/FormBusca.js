7b5058d9efb9f67f063e88af1728dd17ca793511
Gabriel Luna <lunagabri@gmail.com>
2022-11-09 09:12:59 -0300

    Update FormBusca.js

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,13 +1,33 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, Pressable, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const FormBusca = () => {
   return (
     <SafeAreaView>
-      <Text>FormBusca</Text>
+      <Text>Star Trek? O Poderoso Chefão? A Lagoa Azul?</Text>
+      <Text>Localize um filme que você viu ou gostaria de ver</Text>
+      <Pressable
+        style={estilos.botao}
+        // onPress={() => navigation.navigate("Home")}
+      >
+        <Text style={estilos.textoBotao}>
+          <Ionicons name="search" size={16} color="white" />
+          Procurar
+        </Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  botao: {
+    backgroundColor: "#5451a6",
+    textAlign: "center",
+  },
+  textoBotao: {
+    textAlign: "center",
+    color: "white",
+  },
+});
